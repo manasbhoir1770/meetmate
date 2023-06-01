1ba51edfb5de95fec787770026a98d00005dfc1e
zaid <szaid5775@gmail.com>
2023-06-01 15:30:00 +0530

    meetmate added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar(props) {
         <nav className="navbar navbar-expand-lg navbar-dark">
             <div className="container-fluid">
                 <div className="navbar-brand" id= "main-title" >
-                <Link id='mainn' to= "/"><img id='main-title-img' src={require('./sd.png')}alt = "headimg"/>MeetMate</Link>
+                <Link id='mainn' to= "/meetmate"><img id='main-title-img' src={require('./sd.png')}alt = "headimg"/>MeetMate</Link>
                 </div>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
